a070d2a2fdabe976382cc5d84762c1034f007208
Kpax <kpaxstore@gmail.com>
2024-12-16 15:40:31 -0300

    separado o svg para melhorar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import AuthAbstracLeft from '@/assets/svg/AuthAbstractLeft';
+import AuthAbstracRight from '@/assets/svg/AuthAbstractRight';
 import { useTheme } from '@/hooks/useTheme';
 import { Card, Container, Typography, Box } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -31,46 +33,12 @@ export default function EmptyLayout({
           >
             <Box
               display={'flex'}
-              width={'700px'}
+              width={'600px'}
               justifyContent={'center'}
               position='relative'
             >
-              {/* Primeiro SVG */}
-              <Box
-                position={'absolute'}
-                top={'-55px'}
-                left={'-8px'}
-                width={'238px'}
-                height={'239px'}
-                zIndex={-1}
-              >
-                <svg
-                  width='239'
-                  height='234'
-                  viewBox='0 0 239 234'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <rect
-                    x='88.5605'
-                    y='0.700195'
-                    width='149'
-                    height='149'
-                    rx='19.5'
-                    stroke='#7367F0'
-                    stroke-opacity='0.16'
-                  />
-                  <rect
-                    x='0.621094'
-                    y='33.761'
-                    width='200'
-                    height='200'
-                    rx='10'
-                    fill='#7367F0'
-                    fill-opacity='0.08'
-                  />
-                </svg>
-              </Box>
+              {/* SVG left */}
+              <AuthAbstracLeft />
 
               {/* Card de Login */}
               <Card
@@ -81,9 +49,7 @@ export default function EmptyLayout({
                   width: '100%',
                   maxWidth: 460,
                   padding: '2rem',
-                  boxShadow: '0 0.1875rem 0.75rem 0 rgba(47, 43, 61, 0.14)',
-                  background: 'white',
-                  borderRadius: '16px'
+                  boxShadow: '0 0.1875rem 0.75rem 0 rgba(47, 43, 61, 0.14)'
                 }}
               >
                 <Typography
@@ -97,45 +63,8 @@ export default function EmptyLayout({
                 </Typography>
                 {children}
               </Card>
-
-              {/* Segundo SVG */}
-              <Box
-                position={'absolute'}
-                right={'-8px'}
-                bottom={'-55px'}
-                width={'181px'}
-                height={'181px'}
-                zIndex={-1}
-              >
-                <svg
-                  width='181'
-                  height='181'
-                  viewBox='0 0 181 181'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <rect
-                    x='1.30469'
-                    y='1.44312'
-                    width='178'
-                    height='178'
-                    rx='19'
-                    stroke='#7367F0'
-                    stroke-opacity='0.16'
-                    stroke-width='2'
-                    stroke-dasharray='8 8'
-                  />
-                  <rect
-                    x='22.8047'
-                    y='22.9431'
-                    width='135'
-                    height='135'
-                    rx='10'
-                    fill='#7367F0'
-                    fill-opacity='0.08'
-                  />
-                </svg>
-              </Box>
+              {/* SVG right */}
+              <AuthAbstracRight />
             </Box>
           </Container>
         </ThemeProvider>
---
+++ src/assets/svg/AuthAbstractLeft.tsx
@@ -0,0 +1,36 @@
+// src/components/svg/DecorativeSvg.tsx
+export default function AuthAbstracLeft() {
+  return (
+    <svg
+      style={{
+        position: 'absolute',
+        width: '238px',
+        height: '233px',
+        top: '-62px',
+        left: '-5px'
+      }}
+      viewBox='0 0 239 234'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect
+        x='88.5605'
+        y='0.700195'
+        width='149'
+        height='149'
+        rx='19.5'
+        stroke='#7367F0'
+        strokeOpacity='0.16'
+      />
+      <rect
+        x='0.621094'
+        y='33.761'
+        width='200'
+        height='200'
+        rx='10'
+        fill='#7367F0'
+        fillOpacity='0.08'
+      />
+    </svg>
+  );
+}
