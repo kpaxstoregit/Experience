1bb26317e2d726d36f059981f6429082bd016428
Kpax <kpaxstore@gmail.com>
2024-12-16 15:04:47 -0300

    melhorado area de login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useTheme } from '@/hooks/useTheme';
-import { Card, Container, Typography } from '@mui/material';
+import { Card, Container, Typography, Box } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 
@@ -29,31 +29,114 @@ export default function EmptyLayout({
               overflow: 'hidden'
             }}
           >
-            {/* Card de Login */}
-            <Card
-              className='ex-shadow'
-              sx={{
-                position: 'relative',
-                zIndex: 10,
-                width: '100%',
-                maxWidth: 460,
-                padding: '2rem',
-                boxShadow: '0 0.1875rem 0.75rem 0 rgba(47, 43, 61, 0.14)',
-                background: 'white', // Fundo branco para o card
-                borderRadius: '16px' // Border radius opcional
-              }}
+            <Box
+              display={'flex'}
+              width={'700px'}
+              justifyContent={'center'}
+              position='relative'
             >
-              <Typography
-                textAlign='center'
-                fontSize='2rem'
-                fontWeight='700'
-                mb={3}
-                color='primary.main' // Cor primária definida no tema
+              {/* Primeiro SVG */}
+              <Box
+                position={'absolute'}
+                top={'-55px'}
+                left={'-8px'}
+                width={'238px'}
+                height={'239px'}
+                zIndex={-1}
               >
-                Logo
-              </Typography>
-              {children}
-            </Card>
+                <svg
+                  width='239'
+                  height='234'
+                  viewBox='0 0 239 234'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <rect
+                    x='88.5605'
+                    y='0.700195'
+                    width='149'
+                    height='149'
+                    rx='19.5'
+                    stroke='#7367F0'
+                    stroke-opacity='0.16'
+                  />
+                  <rect
+                    x='0.621094'
+                    y='33.761'
+                    width='200'
+                    height='200'
+                    rx='10'
+                    fill='#7367F0'
+                    fill-opacity='0.08'
+                  />
+                </svg>
+              </Box>
+
+              {/* Card de Login */}
+              <Card
+                className='ex-shadow'
+                sx={{
+                  position: 'relative',
+                  zIndex: 10,
+                  width: '100%',
+                  maxWidth: 460,
+                  padding: '2rem',
+                  boxShadow: '0 0.1875rem 0.75rem 0 rgba(47, 43, 61, 0.14)',
+                  background: 'white',
+                  borderRadius: '16px'
+                }}
+              >
+                <Typography
+                  textAlign='center'
+                  fontSize='2rem'
+                  fontWeight='700'
+                  mb={3}
+                  color='primary.main'
+                >
+                  Logo
+                </Typography>
+                {children}
+              </Card>
+
+              {/* Segundo SVG */}
+              <Box
+                position={'absolute'}
+                right={'-8px'}
+                bottom={'-55px'}
+                width={'181px'}
+                height={'181px'}
+                zIndex={-1}
+              >
+                <svg
+                  width='181'
+                  height='181'
+                  viewBox='0 0 181 181'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <rect
+                    x='1.30469'
+                    y='1.44312'
+                    width='178'
+                    height='178'
+                    rx='19'
+                    stroke='#7367F0'
+                    stroke-opacity='0.16'
+                    stroke-width='2'
+                    stroke-dasharray='8 8'
+                  />
+                  <rect
+                    x='22.8047'
+                    y='22.9431'
+                    width='135'
+                    height='135'
+                    rx='10'
+                    fill='#7367F0'
+                    fill-opacity='0.08'
+                  />
+                </svg>
+              </Box>
+            </Box>
           </Container>
         </ThemeProvider>
       </body>
