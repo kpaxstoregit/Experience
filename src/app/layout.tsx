c86d8f774275d894422c4113365f226cbd322da6
Kpax <kpaxstore@gmail.com>
2024-12-16 13:40:43 -0300

    stop a little

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import { useTheme } from '@/hooks/useTheme';
 import { Card, Container, Typography } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
 export default function EmptyLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
-  // Cria o tema dinamicamente com base no estado atual
   const { theme } = useTheme();
 
   return (
@@ -17,23 +17,30 @@ export default function EmptyLayout({
       <body>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+
           <Container
             maxWidth='sm'
             sx={{
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
-              height: '100vh'
+              height: '100vh',
+              position: 'relative',
+              overflow: 'hidden'
             }}
           >
+            {/* Card de Login */}
             <Card
               className='ex-shadow'
               sx={{
+                position: 'relative',
+                zIndex: 10,
                 width: '100%',
                 maxWidth: 460,
                 padding: '2rem',
-
-                boxShadow: '0 0.1875rem 0.75rem 0 rgba(47, 43, 61, 0.14)'
+                boxShadow: '0 0.1875rem 0.75rem 0 rgba(47, 43, 61, 0.14)',
+                background: 'white', // Fundo branco para o card
+                borderRadius: '16px' // Border radius opcional
               }}
             >
               <Typography
@@ -41,6 +48,7 @@ export default function EmptyLayout({
                 fontSize='2rem'
                 fontWeight='700'
                 mb={3}
+                color='primary.main' // Cor primária definida no tema
               >
                 Logo
               </Typography>
